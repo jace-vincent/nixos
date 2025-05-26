201871eaab0381139224fd64a9431a9a788f019c
Jace Vincent <txjacev@gmail.com>
2025-05-25 21:46:02 -0600

    Fix zen-browser.nix: add version parameter and correct SHA256 hash

--- zen-browser.nix
+++ zen-browser.nix
@@ -4,9 +4,10 @@ let
   # Create a custom Zen Browser package using the AppImage
   zen-browser = pkgs.appimageTools.wrapType2 {
     name = "zen-browser";
+    version = "1.0.2-b.3";
     src = pkgs.fetchurl {
       url = "https://github.com/zen-browser/desktop/releases/download/1.0.2-b.3/zen-specific.AppImage";
-      sha256 = "sha256-AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA="; # This will need to be updated
+      sha256 = "sha256-0yphby7kr2l1r2n1srip7rqdllpif0j31f1zwigy54ia5bgd3k7m";
     };
     
     extraPkgs = pkgs: with pkgs; [
