4817bc5bb88cc2664eb327559b19e991e6edc280
Jace Vincent <txjacev@gmail.com>
2025-05-25 21:55:11 -0600

    Fix zen-browser.nix: use correct SRI hash format

--- zen-browser.nix
+++ zen-browser.nix
@@ -7,7 +7,7 @@ let
     version = "1.0.2-b.3";
     src = pkgs.fetchurl {
       url = "https://github.com/zen-browser/desktop/releases/download/1.0.2-b.3/zen-specific.AppImage";
-      hash = "sha256-0yphby7kr2l1r2n1srip7rqdllpif0j31f1zwigy54ia5bgd3k7m";
+      hash = "sha256-9czR3ioqkuJf5D+4MCRw8VLacD43Zh2syIGKPI9f8Ho=";
     };
     
     extraPkgs = pkgs: with pkgs; [
