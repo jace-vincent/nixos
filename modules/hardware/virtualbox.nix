44b2c3b08fdefc85352c196e7574f532d9a6ae94
Jace <txjacev@gmail.com>
2025-05-31 17:03:54 -0600

    removed commented troubleshooting code in virtualbox.nix

--- modules/hardware/virtualbox.nix
+++ modules/hardware/virtualbox.nix
@@ -7,8 +7,4 @@
   # When using the VM we're going to override the boot to a more stable v
   boot.kernelPackages = lib.mkForce pkgs.linuxPackages;
 
-  #  environment.systemPackages = with pkgs; [
-  #    virtualbox-guest-additions
-  #  ];
-
 }
