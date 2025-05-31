1ed22a0076dd48db91937eacf998e9614f1abd19
Jace <txjacev@gmail.com>
2025-05-31 14:40:04 -0600

    renamed /users/jacev/userconfig.nix to users/jacev/config.nix

--- configuration.nix
+++ configuration.nix
@@ -9,7 +9,7 @@
     [ # Include the results of the hardware scan.
       ./hardware-configuration.nix
       ./modules/hardware/bootloader.nix
-      ./users/jacev/userconfig.nix
+      ./users/jacev/config.nix
       ./system/config.nix
     ];
 
