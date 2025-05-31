00e09a70c177dc29b37f91720c74d58acabf2e5b
Jace <txjacev@gmail.com>
2025-05-31 13:45:45 -0600

    removed .gitignore generation from nix files in favor of just pulling that from github

--- modules/programs/git.nix
+++ modules/programs/git.nix
@@ -1,15 +1,15 @@
 {config, pkgs, ...}:
 
-  { 
-    programs.git = {
-      enable = true;
-      config = {
-        credential.helper = "store";
-        init.defaultBranch = "main";
-	user = {
-	  Name = "Jace Vincent";
-	  Email = "txjacev@gmail.com";
-        };
+{ 
+  programs.git = {
+    enable = true;
+    config = {
+      credential.helper = "store";
+      init.defaultBranch = "main";
+      user = {
+        Name = "Jace Vincent";
+        Email = "txjacev@gmail.com";
       };
     };
-  }
+  };
+}  
