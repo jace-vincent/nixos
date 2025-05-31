0c3e48f9cb8d751804cd6c750544a817f135dc36
Jace <txjacev@gmail.com>
2025-05-31 11:58:21 -0600

    moved audio config to be system wide referencing modules/hardware/audio.nix

--- system/config.nix
+++ system/config.nix
@@ -4,8 +4,9 @@
     # imports in our system/config file are mostly just programs we want to set some default configs on
     # that go a bit beyond simply installing them as a package
     imports = [
-    ../modules/programs/firefox.nix
-    ../modules/programs/git.nix
+    ../modules/hardware/audio.nix # very important! 
+    ../modules/programs/firefox.nix # our default system browser
+    ../modules/programs/git.nix # everybody needs git
     ];
 
     # List packages installed in system profile. To search, run:
