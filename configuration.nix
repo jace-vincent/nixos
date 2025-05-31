a535bb52108a175796b250e6cd37943bf3f3a6a5
Jace <txjacev@gmail.com>
2025-05-31 12:40:00 -0600

    moved x11 to its own service module referenced in basic theme

--- configuration.nix
+++ configuration.nix
@@ -49,18 +49,17 @@
   };
 
   # Enable the X11 windowing system.
-  services.xserver.enable = true;
+  # services.xserver.enable = true;
+
+  # Configure keymap in X11
+  # services.xserver.xkb = {
+  #   layout = "us";
+  #   variant = "";
+  # };
 
   # Enable the GNOME Desktop Environment.
   #  services.xserver.displayManager.gdm.enable = true;
   #  services.xserver.desktopManager.gnome.enable = true;
-
-  # Configure keymap in X11
-  services.xserver.xkb = {
-    layout = "us";
-    variant = "";
-  };
-
   # Enable CUPS to print documents.
   #  services.printing.enable = true;
 
--- themes/basic.nix
+++ themes/basic.nix
@@ -1,8 +1,9 @@
 {config, pkgs, ...}:
 
 {
-
+  # A very spartan X11 and GNOME setup for servers or hardware tests
   imports = [
+  ../modules/services/x11.nix
   ../modules/desktops/gnome.nix
   ];
   
