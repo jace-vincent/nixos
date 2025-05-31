9d16ab36839852dc2cc805f80dec031607760183
Jace <txjacev@gmail.com>
2025-05-31 12:19:49 -0600

    moved CUPS printing setup to modules/services/printing.nix and put in system/config setup

--- system/config.nix
+++ system/config.nix
@@ -5,6 +5,7 @@
     # that go a bit beyond simply installing them as a package
     imports = [
     ../modules/hardware/audio.nix # very important! 
+    ../modules/services/printing.nix # not required but very useful
     ../modules/programs/firefox.nix # our default system browser
     ../modules/programs/git.nix # everybody needs git
     ];
