5ad166424d330486e779aa40a9b2b30225469d5b
Jace <txjacev@gmail.com>
2025-05-31 20:22:16 -0600

    added context menu to mouse right click

--- themes/kde/black-plasma.nix
+++ themes/kde/black-plasma.nix
@@ -26,43 +26,13 @@
         right = [ "minimize"  "maximize"  "close" ];
       };
     };
-  };
 
-  # Configure KDE settings via dconf/files
-  #  home.file.".config/kdeglobals".text = '' 
-  #    [ColorScheme]
-  #    ColorScheme=BreezeDark
-  #
-  #    [General]
-  #    ColorScheme=BreezeDark
-  #    Name=Breeze Dark
-  #
-  #    [Icons]
-  #    Theme=breeze-dark
-  #
-  #    [KDE]
-  #    LookAndFeelPackage=org.kde.breezedark.desktop
-  #    widgetStyle=breeze
-  #    '';
-  #
-  #    home.file.".config/kwinrc".text = ''
-  #      [org.kde.kdecoration2]
-  #      library=org.kde.breeze
-  #      theme=Breeze
-  #
-  #      [Compositing]
-  #      OpenGLIsUnsafe=false
-  #
-  #      [Effect-overview]
-  #      BorderActivate-9
-  #    '';
-  #
-  #      # GTK theme for non-KDE apps
-  #    home.file.".config/gtk-3.0/settings.ini".text = ''
-  #      [Settings]
-  #      gtk-theme-name=Breeze-Dark
-  #      gtk-icon-theme-name=breeze-dark
-  #      gtk-application-prefer-dark-theme=1
-  #    '';
-  #
+    # Desktop and panels configuration
+    desktop = {
+      mouseActions = {
+      rightClick = "contextMenu";
+      };
+    };
+  };
 }
+
