4ae76347b4d249c13ff9d134001ef8100933b138
Jace Vincent <txjacev@gmail.com>
2025-06-11 15:25:23 -0600

    fixed missing areas of flake.nix to allow for truly a truly parametric lifestyle

--- flake.nix
+++ flake.nix
@@ -1,132 +1,127 @@
-# Flake.nix is a file that is required to generate the flake.lock files 
-# These flake.lock files are what enables Flakes to have great version control!
 {
   description = "My flakey place";
-    
-      outputs = inputs@{ self, ...}: 
-        let 
-          # ---- SYSTEM SETTINGS ---- #
-          systemSettings = {
-            system = "x86_64-linux";
-            profile = "personal"; # Select personal or work
-            timezone = "America/Denver"; # Select timezone
-            locale = "en_US.UTF-8"; # Select locale
-            bootMode = "bios"; # Select Bios or UEFI
-            gpuType = "nvidia"; # Select nvidia or amd
-          };
-    
-          # ---- USER SETTINGS ---- #
-          userSettings = rec {
-            username = "jace"; # your "gamer" name
-            name = "Jace"; # your "real" name
-            email = "txjacev@gmail.com"; # email
-            dotfilesDit = "~/.dotfiles"; # absolute path of the local repo
-            theme = ""; # Select from themes dir (./themes/)
-            wm = "kde"; # Select from desktops dir (./desktops)
-            browser = "firefox"; # Select from browsers dir (./programs)
-            terminal = "alacritty"; # Select your default terminal
-            font = "Intel One Mono"; # S, elect font
-            fontPkg = pkgs.intel-one-mono; # Package for your selected font
-            editor = "nvim"; # Select your default text editor
-            spawnEditor = "exec " + terminal + " -e" + editor;
-          };
 
-			
-        # Here is where we define pkgs based on our system profile
-        pkgs = (if ((systemSettings.profile == "homelab") || (systemSettings.profile == "work"))
-               then 
-  	       pkgs-stable
-	       else
-  	       pkgs-unstable
-  	);
-  
+  outputs = inputs@{ self, ... }:
+    let
+      systemSettings = {
+        system = "x86_64-linux";
+        profile = "personal";
+        timezone = "America/Denver";
+        locale = "en_US.UTF-8";
+        bootMode = "bios";
+        gpuType = "nvidia";
+      };
+
+      userSettings = rec {
+        username = "jace";
+        name = "Jace";
+        email = "txjacev@gmail.com";
+        dotfilesDir = "~/.dotfiles"; # fixed typo
+        theme = "";
+        wm = "kde";
+        browser = "firefox";
+        terminal = "alacritty";
+        font = "Intel One Mono";
+        editor = "nvim";
+        spawnEditor = "exec " + terminal + " -e " + editor;
+      };
+
       pkgs-stable = import inputs.nixpkgs-stable {
         system = systemSettings.system;
-	  config = {
-	    allowUnfree = true;
-	    allowUnfreePredicate = (_: true);
-	  };
+        config = {
+          allowUnfree = true;
+          allowUnfreePredicate = (_: true);
+        };
       };
 
       pkgs-unstable = import inputs.nixpkgs-unstable {
         system = systemSettings.system;
-	  config = {
-	    allowUnfree = true;
-	    allowUnfreePredicate = (_: true);
-	  };
-      };
-
-
-      # Here is where we define our nixpkgs available in lib based on our seleted system profile
-      lib = (if ((systemSettings.profile == "homelab") || (systemSettings.profile == "work"))
-             then
-               inputs.nixpkgs-stable.lib
-             else
-               inputs.nixpkgs-unstable.lib);
-	       
-
-      # Here is where we define home-manager based on our selected system profile
-      home-manager = (if ((systemSettings.profile == "homelab") || (systemSettings.profile == "work"))
-                      then 
-		        inputs.home-manager-stable
-		      else
-			inputs.home-manager-unstable
-		      );
-
-      # Here is where we define home-manager based on our selected system profile
-      plasma-manager = (if ((systemSettings.profile == "homelab") || (systemSettings.profile == "work"))
-                      then 
-		        inputs.plasma-manager-stable
-		      else
-			inputs.plasma-manager-unstable
-		      );
-
-      in {
-        nixosConfigurations = {
-	  nixos = lib.nixosSystem {
-            system = systemSettings.system;
-            modules = [
-              ./configuration.nix
-        
-              # Add Home Manager module
-              home-manager.nixosModules.home-manager {
-                home-manager.useGlobalPkgs = true;
-                home-manager.useUserPackages = true;
-                home-manager.users.jacev = import ./users/jacev/home.nix; 
-		# Required to use plasma-manager
-                home-manager.sharedModules = [ plasma-manager.homeManagerModules.plasma-manager ];
-              }
-            ];
-	  };
+        config = {
+          allowUnfree = true;
+          allowUnfreePredicate = (_: true);
         };
       };
 
-  inputs = {
+      pkgs = if ((systemSettings.profile == "homelab") || (systemSettings.profile == "work"))
+        then pkgs-stable
+        else pkgs-unstable;
 
-    nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
+      lib = if ((systemSettings.profile == "homelab") || (systemSettings.profile == "work"))
+        then inputs.nixpkgs-stable.lib
+        else inputs.nixpkgs-unstable.lib;
+
+      home-manager = if ((systemSettings.profile == "homelab") || (systemSettings.profile == "work"))
+        then inputs.home-manager-stable
+        else inputs.home-manager-unstable;
+
+      plasma-manager = if ((systemSettings.profile == "homelab") || (systemSettings.profile == "work"))
+        then inputs.plasma-manager-stable
+        else inputs.plasma-manager-unstable;
+
+      supportedSystems = [ "aarch64-linux" "i686-linux" "x86_64-linux" ];
+
+      forAllSystems = inputs.nixpkgs-unstable.lib.genAttrs supportedSystems;
+
+      nixpkgsFor = forAllSystems (system:
+        if ((systemSettings.profile == "homelab") || (systemSettings.profile == "work"))
+          then import inputs.nixpkgs-stable { inherit system; }
+          else import inputs.nixpkgs-unstable { inherit system; }
+      );
+    in
+    {
+      nixosConfigurations = {
+        nixos = lib.nixosSystem {
+          system = systemSettings.system;
+          modules = [ ./configuration.nix ];
+          specialArgs = {
+            inherit pkgs-stable;
+            inherit pkgs-unstable;
+            fontPkg = pkgs.intel-one-mono;
+            inherit systemSettings;
+            inherit userSettings;
+            inherit inputs;
+          };
+        };
+      };
+
+      packages = forAllSystems (system:
+        let pkgs = nixpkgsFor.${system};
+        in {
+          default = self.packages.${system}.install;
+
+          install = pkgs.writeShellApplication {
+            name = "install";
+            runtimeInputs = with pkgs; [ git ];
+            text = ''${./install.sh} "$@"'';
+          };
+        }
+      );
+    };
+
+  inputs = {
+    nixpkgs-unstable.url = "nixpkgs/nixos-unstable";
     nixpkgs-stable.url = "nixpkgs/nixos-24.05";
 
     home-manager-unstable = {
       url = "github:nix-community/home-manager/master";
-      inputs.nixpkgs-unstable.follows = "nixpkgs-unstable";
+      inputs.nixpkgs.follows = "nixpkgs-unstable";
     };
 
     home-manager-stable = {
-      url = "github:nix-community/home-manager/master";
-      inputs.nixpkgs-unstable.follows = "nixpkgs-stable";
+      url = "github:nix-community/home-manager/release-24.05";
+      inputs.nixpkgs.follows = "nixpkgs-stable";
     };
 
     plasma-manager-unstable = {
       url = "github:pjones/plasma-manager";
-      inputs.nixpkgs-unstable.follows = "nixpkgs-unstable";
-      inputs.home-manager-unstable.follows = "home-manager-unstable";
+      inputs.nixpkgs.follows = "nixpkgs-unstable";
+      inputs.home-manager.follows = "home-manager-unstable";
     };
 
     plasma-manager-stable = {
       url = "github:pjones/plasma-manager";
-      inputs.nixpkgs-stable.follows = "nixpkgs-stable";
-      inputs.home-manager-stable.follows = "home-manager-stable";
+      inputs.nixpkgs.follows = "nixpkgs-stable";
+      inputs.home-manager.follows = "home-manager-stable";
     };
-
   };
 }
