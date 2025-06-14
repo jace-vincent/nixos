95d334efd9d98595b59120bbe9283a39515a9b43
Jace Vincent <txjacev@gmail.com>
2025-06-14 13:27:16 -0600

    added git-credential-libsecret helper library

--- users/jacev/home.nix
+++ users/jacev/home.nix
@@ -1,15 +1,18 @@
-{ config, pkgs, userSettings, ... }: {
+{ config, pkgs, userSettings, ... }: 
+
+{
   programs.git = {
     enable = true;
     userName = userSettings.name;    # Uses your flake variable
     userEmail = userSettings.email;  # Uses your flake variable
     extraConfig = {
-      credential.helper = "${pkgs.git}/bin/git-credential-libsecret";
+      credential.helper = "${pkgs.git-credential-libsecret}/bin/git-credential-libsecret";
     };
   };
 
   home.packages = with pkgs; [
     libsecret
+    git-credential-libsecret
     gnome.seahorse
   ];
 
