69cb0b903f87c17f8520959b1b35a1ef709f94f7
Jace Vincent <txjacev@gmail.com>
2025-06-18 16:52:16 -0600

    added python extension to vscode module

--- users/jacev/modules/vscode.nix
+++ users/jacev/modules/vscode.nix
@@ -3,4 +3,13 @@
   home.packages = with pkgs; [
     vscode
   ];
+
+  programs.vscode = {
+  enable = true;
+  profiles.default = {
+    extensions = with pkgs.vscode-extensions; [
+      ms-python.python
+      ];
+    };
+  };
 }
